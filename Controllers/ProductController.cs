9be5a48d40970e9b39c7e0e633dea7bf4df32722
krish-mamtora <krish.mamtora@roimaint.com>
2026-02-04 22:02:31 +0530

    fix CSRF by adding anti-forgery token

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -25,6 +25,7 @@ public class ProductController : Controller
         return View();
     }
     [HttpPost]
+    [ValidateAntiForgeryToken]
     public IActionResult Create(Product product)
     {
         Console.WriteLine("Post Creating ..");
